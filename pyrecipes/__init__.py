048696b65dbf7c2f29ad737d57fa918252443961
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2017-09-11 15:58:36 -0400

    Fixed module reference in __init__

--- pyrecipes/__init__.py
+++ pyrecipes/__init__.py
@@ -1,3 +1,3 @@
-from .mpl_images import *
+from .mpl_annotate import *
 from .mpl_pes import *
 from .mpl_axis import *
--- setup.py
+++ setup.py
@@ -2,18 +2,13 @@ from setuptools import setup
 
 setup(
     name="pyrecipes",
-    version="0.3",
+    version="0.1",
     description="A collection of Python 3 functions and recipes for figure making.",
     author="Kelvin Lee",
     packages=["pyrecipes"],
     author_email="kin.long.kelvin.lee@gmail.com",
     install_requires=[
             "numpy",
-            "pandas",
-            "scipy",
-            "peakutils",
-            "seaborn",
-            "colorlover",
             "matplotlib"
     ]
 )
